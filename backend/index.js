8b03e1039f1ad2500b5b29d8ff64abb11d73d16c
RyAndroid <ryan.w.simpson@gmail.com>
2024-10-26 15:38:24 +0200

    update 2 backend port config

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,17 @@
 const express = require('express'),
   path = require('path');
 
-app.use(express.static(path.join(path.resolve(), 'dist')));
+const app = express();
 
-const app = express(),
-  port = process.env.PORT || 3000;
-
-app.get('/', (_request, response) => {
-  response.send({ hello: 'World' });
-});
-
-app.listen(port, () => {
-  console.log(`Redo på http://localhost:${port}/`);
-});
 const dotenv = require('dotenv'),
   { Client } = require('pg');
 
 app.get('/api', (_request, response) => {
   response.send({ emojis: '🫡😜😶‍🌫️🫨😌😪' });
+});
+
+app.use(express.static(path.join(path.resolve(), 'dist')));
+
+app.listen(3000, () => {
+  console.log('Redo på http://localhost:3000/');
 });
