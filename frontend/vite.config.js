fb14ed019851d60b1e72491fa6e89515a202c01e
RyAndroid <ryan.w.simpson@gmail.com>
2024-10-26 16:03:58 +0200

    update 3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,2 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000',
-    },
-  },
-});
