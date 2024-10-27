15daae30c6de12c18bcf68382fec85b8c7f0eca3
RyAndroid <ryan.w.simpson@gmail.com>
2024-10-27 20:34:28 +0100

    styling changes

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express'),
   path = require('path');
 
 const app = express();
-const port = process.env.PORT || 4000; // Bind to the PORT environment variable
+const port = process.env.PORT || 3000;
 
 app.get('/api', (_request, response) => {
   response.send({ hello: 'World' });
