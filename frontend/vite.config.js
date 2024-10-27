428a16ee84cc3d63ee079868315505193c6fb2f2
RyAndroid <ryan.w.simpson@gmail.com>
2024-10-27 19:42:19 +0100

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,5 +8,10 @@ export default defineConfig({
     rollupOptions: {
       external: ['emoji-picker-react'],
     },
+    server: {
+      proxy: {
+        '/api': 'http://localhost:3000',
+      },
+    },
   },
 });
